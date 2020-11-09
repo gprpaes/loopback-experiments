b13e27aaeae2aa94ac7d577b0d807f69eb6d8211
Guilherme Paes <guilhermep98@gmail.com>
2020-11-09 02:10:01 -0300

    added remote method

--- lb3/common/models/jogo.js
+++ lb3/common/models/jogo.js
@@ -2,4 +2,24 @@
 
 module.exports = function(Jogo) {
 
+    Jogo.poze = function(){
+        //TODO Add random poze rant videos
+        console.log('poze')
+        return {video: "https://twitter.com/i/status/1325588054111899648"}
+    }
+
+    Jogo.remoteMethod(
+        'poze', {
+          http: {
+            path: '/poze',
+            verb: 'get'
+          },
+          returns: {
+            arg: 'poze',
+            type: 'string'
+          }
+        }
+      );
+    
 };
+
